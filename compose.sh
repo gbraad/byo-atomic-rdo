de56cee4f91912379a492335b8fbac80e150b206
Gerard Braad <me@gbraad.nl>
2016-07-08 06:29:24 +0000

    Project link page (prevent 404)

--- compose.sh
+++ compose.sh
@@ -11,6 +11,11 @@ rpm-ostree compose tree \
     --repo=/srv/repo \
     ./rdo-atomic/rdo-atomic-host.json
 
-find /srv/repo/ -type f | ./genindex.sh > /srv/repo/index.html
+#find /srv/repo/ -type f | ./genindex.sh > /srv/repo/index.html
+cat << EOF > /srv/repo/index.html
+<html><body>
+<a href="https://gitlab.com/gbraad/byo-atomic-rdo/">byo-atomic-rdo</a>
+</body></html
+EOF
 
 popd
